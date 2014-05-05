47c7ab8d9eb2aedda4753904979a357859fd1b15
icburns <ebeptonian@gmail.com>
2014-05-04 21:05:34 -0700

    Migrated to html file. Post tagging is visually indicated
    Since we're not using PHP (Was replaced by Node.js and Socket.io a few
    days in), the file now bears .html extension.
    Tag buttons are given color when the tagging action is complete.

--- backchannel.js
+++ backchannel.js
@@ -1,17 +1,77 @@
 "use strict";
 (function()
 {
-    $(document).ready(function() {
-        $('form[name=makeNewPost]').submit(function(e){
-            e.preventDefault();
-            console.log("post="+$('input[name=newPost]')[0].value);
-            $.ajax({
-                type: 'POST',
-                url: '',
-                data: 'post='+$('input[name=newPost]')[0].value, 
-                success: function(msg) {
-                }
-            });
-        });     
-    });      
+	$(document).ready(function(){
+	
+		$('#questionTag').click(function() {
+			tagPost('question','rgba(100,100,200,1)');
+		});
+		$('#resourceTag').click(function() {
+			tagPost('resource','rgba(100,200,100,1)');
+		});
+	
+		function tagPost(tagValue,tagColor){
+			$("#resourceTag").css('background-color','transparent');
+			$("#questionTag").css('background-color','transparent');
+			if($('#postTag').val()==tagValue){
+				tagColor = 'transparent';
+				tagValue = '';
+			}
+			$("#"+tagValue+"Tag").css('background-color',tagColor);
+			$('#postTag').val(tagValue);
+		}
+	
+		var socket = io.connect('http://localhost:8000');
+		var postForm = $('form[name=makeNewPost]');
+		var backchannel = $('#backchannel');
+		var postArea = $('#postArea');
+		
+		postForm.submit(function(e){
+			e.preventDefault();
+			socket.emit('send', {"post":postArea.val(), "name":socket.socket.sessionid, "tag":$('#postTag').val()});
+			postArea.val('');
+		});
+		
+		socket.on('post', function(msg){
+			$('#postTag').val('');
+			var newMessage = $('<div/>', {				
+				text: msg.who+": \'"+msg.post+"\' ("+msg.tag+")"
+			}).appendTo(backchannel);
+		});
+		
+		$("#username").keyup(function(e){
+		if(e.which == 13) {
+			var name = $("#username").val();
+			if (name != "") {			
+				socket.emit("join", name);
+				$("#login").detach();
+			}
+		}
+	});
+
+
+//will probably ditch this functionality as it would be cumbersome for backchannel
+	socket.on("update", function(msg) {
+			var newMessage = $('<div/>', {				
+				text: msg
+			}).appendTo("#backchannel");
+	});
+
+// will repopulate users on a (dis)connection
+	socket.on("update-users", function(currentUsers){
+			$("#users").empty();
+			$.each(currentUsers, function(clientid, name) {
+				$('#users').append(name);
+			});
+	});
+
+	socket.on("disconnect", function(){
+		$("#msgs").append("The server is not available");
+		$("#msg").attr("disabled", "disabled");
+		$("#send").attr("disabled", "disabled");
+	});
+
+});
+
+     
 })();
